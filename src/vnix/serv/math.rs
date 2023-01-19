e57ac7def15c15897522a493e049f6cbd062cf88
Oleg Sayakhov <gluhoy1324@gmail.com>
2023-01-20 02:44:48 +0500

    math.int: refactored + support for nested ops

--- src/vnix/serv/math.rs
+++ src/vnix/serv/math.rs
@@ -1,5 +1,7 @@
-use alloc::vec::Vec;
 use alloc::vec;
+use alloc::vec::Vec;
+use alloc::boxed::Box;
+use alloc::string::String;
 
 use crate::vnix::core::msg::Msg;
 
@@ -8,21 +10,51 @@ use crate::vnix::core::kern::KernErr;
 use crate::vnix::core::unit::Unit;
 
 
-pub enum Operation {
-    Neg(i32),
-    Abs(i32),
-    Inc(i32),
-    Dec(i32),
-    Sqr(i32),
-    Sqrt(i32),
-    Fac(i32),
-    Sum(Vec<i32>),
-    Sub(Vec<i32>),
-    Mul(Vec<i32>),
-    Div(Vec<i32>),
-    Mod(Vec<i32>),
-    Pow(Vec<i32>),
+#[derive(Debug)]
+pub enum Operand {
+    Int(i32),
+    Operation(Box<Operation>)
 }
+
+#[derive(Debug, Clone)]
+pub enum SingleOpAct {
+    Neg,
+    Abs,
+    Inc,
+    Dec,
+    Sqr,
+    Sqrt,
+    Fac
+}
+
+#[derive(Debug)]
+pub struct SingleOp {
+    act: SingleOpAct,
+    op: Operand
+}
+
+#[derive(Debug, Clone)]
+pub enum MultiOpAct {
+    Sum,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Pow,
+}
+
+#[derive(Debug)]
+pub struct MultiOp {
+    act: MultiOpAct,
+    op: Vec<Operand>
+}
+
+#[derive(Debug)]
+pub enum Operation {
+    Single(SingleOp),
+    Multi(MultiOp)
+}
+
 
 pub struct Int {
     op: Option<Operation>
@@ -36,199 +68,181 @@ impl Default for Int {
     }
 }
 
+impl Int {
+    fn find_single_op_with(path: Vec<String>, act: SingleOpAct, u: &Unit) -> Option<Operation> {
+        let op = u.find_int(&mut path.iter()).map(|v| {
+            Operation::Single(
+                SingleOp {
+                    act: act.clone(),
+                    op: Operand::Int(v)
+                }
+            )
+        });
+
+        if op.is_some() {
+            return op;
+        }
+
+        let op = u.find_map(&mut path.iter()).map(|m| {
+            Some(
+                Operation::Single(
+                    SingleOp {
+                        act: act,
+                        op: Operand::Operation(Box::new(Int::find_op(&Unit::Map(m))?))
+                    }
+                )
+            )
+        }).flatten();
+
+        op
+    }
+
+    fn find_single_op(u: &Unit) -> Option<Operation> {
+        let ops = vec![
+            (vec!["neg".into()], SingleOpAct::Neg),
+            (vec!["abs".into()], SingleOpAct::Abs),
+            (vec!["inc".into()], SingleOpAct::Inc),
+            (vec!["dec".into()], SingleOpAct::Dec),
+            (vec!["sqr".into()], SingleOpAct::Sqr),
+            (vec!["sqrt".into()], SingleOpAct::Sqrt),
+            (vec!["fac".into()], SingleOpAct::Fac)
+        ];
+
+        ops.iter().find_map(|(path, act)| Int::find_single_op_with(path.clone(), act.clone(), u))
+    }
+
+    fn find_multi_op_with(path: Vec<String>, act: MultiOpAct, u: &Unit) -> Option<Operation> {
+        let op = u.find_pair(&mut path.iter()).map(|(u0, u1)| {
+            let v0 = match u0 {
+                Unit::Int(v) => Operand::Int(v),
+                Unit::Map(m) => Operand::Operation(Box::new(Int::find_op(&Unit::Map(m))?)),
+                _ => return None
+            };
+
+            let v1 = match u1 {
+                Unit::Int(v) => Operand::Int(v),
+                Unit::Map(m) => Operand::Operation(Box::new(Int::find_op(&Unit::Map(m))?)),
+                _ => return None
+            };
+
+            Some(Operation::Multi(
+                MultiOp {
+                    act: act.clone(),
+                    op: vec![v0, v1]
+                }
+            ))
+        }).flatten();
+
+        if op.is_some() {
+            return op;
+        }
+
+        let op = u.find_list(&mut path.iter()).map(|lst| {
+            let lst = lst.iter().cloned().map(|u| {
+                match u {
+                    Unit::Int(v) => Some(Operand::Int(v)),
+                    Unit::Map(m) => Some(Operand::Operation(Box::new(Int::find_op(&Unit::Map(m))?))),
+                    _ => return None
+                }
+            }).filter_map(|v| v);
+
+            Operation::Multi(
+                MultiOp {
+                    act: act.clone(),
+                    op: lst.collect()
+                }
+            )
+        });
+
+        op
+    }
+
+    fn find_multi_op(u: &Unit) -> Option<Operation> {
+        let ops = vec![
+            (vec!["sum".into()], MultiOpAct::Sum),
+            (vec!["sub".into()], MultiOpAct::Sub),
+            (vec!["mul".into()], MultiOpAct::Mul),
+            (vec!["div".into()], MultiOpAct::Div),
+            (vec!["mod".into()], MultiOpAct::Mod),
+            (vec!["pow".into()], MultiOpAct::Pow)
+        ];
+
+        ops.iter().find_map(|(path, act)| Int::find_multi_op_with(path.clone(), act.clone(), u))
+    }
+
+    fn find_op(u: &Unit) -> Option<Operation> {
+        let op = Int::find_single_op(u);
+        if op.is_some() {
+            return op;
+        }
+
+        Int::find_multi_op(u)
+    }
+
+    fn calc_op(op: &Operation) -> i32 {
+        match op {
+            Operation::Single(op) => Int::calc_single_op(op),
+            Operation::Multi(op) => Int::calc_multi_op(op)
+        }
+    }
+
+    fn calc_single_op(op: &SingleOp) -> i32 {
+        let v = match op.op {
+            Operand::Int(v) => v,
+            Operand::Operation(ref op) => Int::calc_op(&op)
+        };
+
+        match op.act {
+            SingleOpAct::Neg => -v,
+            SingleOpAct::Abs => v.abs(),
+            SingleOpAct::Inc => v + 1,
+            SingleOpAct::Dec => v - 1,
+            SingleOpAct::Sqr => v * v,
+            SingleOpAct::Sqrt => libm::truncf(libm::sqrtf(v as f32)) as i32,
+            SingleOpAct::Fac => (1..=v).product()
+        }
+    }
+
+    fn calc_multi_op(op: &MultiOp) -> i32 {
+        let lst = op.op.iter().map(|op| {
+            match op {
+                Operand::Int(v) => *v,
+                Operand::Operation(ref op) => Int::calc_op(&op)
+            }
+        });
+
+        lst.reduce(|a, b| {
+            match op.act {
+                MultiOpAct::Sum => a + b,
+                MultiOpAct::Sub => a - b,
+                MultiOpAct::Pow => a.pow(b as u32),
+                MultiOpAct::Mul => a * b,
+                MultiOpAct::Div => a / b,
+                MultiOpAct::Mod => a % b
+            }
+        }).unwrap_or(0)
+    }
+}
+
 impl ServHlr for Int {
     fn inst(msg: Msg, _serv: &mut Serv) -> Result<(Self, Msg), KernErr> {
         let mut inst = Int::default();
 
         // config instance
-        msg.msg.find_int(&mut vec!["neg".into()].iter()).map(|v| inst.op = Some(Operation::Neg(v)));
-        msg.msg.find_int(&mut vec!["abs".into()].iter()).map(|v| inst.op = Some(Operation::Abs(v)));
-        msg.msg.find_int(&mut vec!["inc".into()].iter()).map(|v| inst.op = Some(Operation::Inc(v)));
-        msg.msg.find_int(&mut vec!["dec".into()].iter()).map(|v| inst.op = Some(Operation::Dec(v)));
-        msg.msg.find_int(&mut vec!["sqr".into()].iter()).map(|v| inst.op = Some(Operation::Sqr(v)));
-        msg.msg.find_int(&mut vec!["sqrt".into()].iter()).map(|v| inst.op = Some(Operation::Sqrt(v)));
-        msg.msg.find_int(&mut vec!["fac".into()].iter()).map(|v| inst.op = Some(Operation::Fac(v)));
-
-        msg.msg.find_pair(&mut vec!["sum".into()].iter())
-            .filter(|(u0, u1)| u0.as_int().is_some() && u1.as_int().is_some())
-            .map(|(u0, u1)| inst.op = Some(Operation::Sum(vec![u0.as_int().unwrap(), u1.as_int().unwrap()])));
-
-        msg.msg.find_list(&mut vec!["sum".into()].iter()).map(|lst| {
-            let out = lst.iter().filter_map(|u| u.as_int()).collect();
-            inst.op = Some(Operation::Sum(out));
-        });
-
-        msg.msg.find_pair(&mut vec!["sub".into()].iter())
-            .filter(|(u0, u1)| u0.as_int().is_some() && u1.as_int().is_some())
-            .map(|(u0, u1)| inst.op = Some(Operation::Sub(vec![u0.as_int().unwrap(), u1.as_int().unwrap()])));
-
-        msg.msg.find_list(&mut vec!["sub".into()].iter()).map(|lst| {
-                let out = lst.iter().filter_map(|u| u.as_int()).collect();
-                inst.op = Some(Operation::Sub(out));
-            });
-
-        msg.msg.find_pair(&mut vec!["mul".into()].iter())
-            .filter(|(u0, u1)| u0.as_int().is_some() && u1.as_int().is_some())
-            .map(|(u0, u1)| inst.op = Some(Operation::Mul(vec![u0.as_int().unwrap(), u1.as_int().unwrap()])));
-
-        msg.msg.find_list(&mut vec!["mul".into()].iter()).map(|lst| {
-                let out = lst.iter().filter_map(|u| u.as_int()).collect();
-                inst.op = Some(Operation::Mul(out));
-            });
-
-        msg.msg.find_pair(&mut vec!["div".into()].iter())
-            .filter(|(u0, u1)| u0.as_int().is_some() && u1.as_int().is_some())
-            .map(|(u0, u1)| inst.op = Some(Operation::Div(vec![u0.as_int().unwrap(), u1.as_int().unwrap()])));
-
-        msg.msg.find_list(&mut vec!["div".into()].iter()).map(|lst| {
-                let out = lst.iter().filter_map(|u| u.as_int()).collect();
-                inst.op = Some(Operation::Div(out));
-            });
-
-        msg.msg.find_pair(&mut vec!["mod".into()].iter())
-            .filter(|(u0, u1)| u0.as_int().is_some() && u1.as_int().is_some())
-            .map(|(u0, u1)| inst.op = Some(Operation::Mod(vec![u0.as_int().unwrap(), u1.as_int().unwrap()])));
-
-        msg.msg.find_list(&mut vec!["mod".into()].iter()).map(|lst| {
-                let out = lst.iter().filter_map(|u| u.as_int()).collect();
-                inst.op = Some(Operation::Mod(out));
-            });
-
-        msg.msg.find_pair(&mut vec!["pow".into()].iter())
-            .filter(|(u0, u1)| u0.as_int().is_some() && u1.as_int().is_some())
-            .map(|(u0, u1)| inst.op = Some(Operation::Pow(vec![u0.as_int().unwrap(), u1.as_int().unwrap()])));
-
-        msg.msg.find_list(&mut vec!["pow".into()].iter()).map(|lst| {
-            let out = lst.iter().filter_map(|u| u.as_int()).collect();
-            inst.op = Some(Operation::Pow(out));
-        });
+        inst.op = Int::find_op(&msg.msg);
 
         return Ok((inst, msg))
     }
 
     fn handle(&self, msg: Msg, serv: &mut Serv) -> Result<Option<Msg>, KernErr> {
         if let Some(ref op) = self.op {
-            if let Operation::Neg(v) = op {
-                let m = vec![
-                    (Unit::Str("msg".into()), Unit::Int(-v)),
-                ];
-    
-                return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-            }
+            let out = Int::calc_op(op);
 
-            if let Operation::Abs(v) = op {
-                let m = vec![
-                    (Unit::Str("msg".into()), Unit::Int(v.abs())),
-                ];
-    
-                return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-            }
+            let m = vec![
+                (Unit::Str("msg".into()), Unit::Int(out)),
+            ];
 
-            if let Operation::Inc(v) = op {
-                let m = vec![
-                    (Unit::Str("msg".into()), Unit::Int(v + 1)),
-                ];
-    
-                return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-            }
-    
-            if let Operation::Dec(v) = op {
-                let m = vec![
-                    (Unit::Str("msg".into()), Unit::Int(v - 1)),
-                ];
-    
-                return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-            }
-
-            if let Operation::Sqr(v) = op {
-                let m = vec![
-                    (Unit::Str("msg".into()), Unit::Int(v * v)),
-                ];
-    
-                return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-            }
-
-            if let Operation::Sqrt(v) = op {
-                let m = vec![
-                    (Unit::Str("msg".into()), Unit::Int(libm::truncf(libm::sqrtf(*v as f32)) as i32)),
-                ];
-    
-                return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-            }
-
-            if let Operation::Fac(v) = op {
-                let m = vec![
-                    (Unit::Str("msg".into()), Unit::Int((1..=*v).product())),
-                ];
-    
-                return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-            }
-
-            if let Operation::Sum(lst) = op {
-                let out = lst.iter().cloned().reduce(|a, b| a + b);
-
-                if let Some(out) = out {
-                    let m = vec![
-                        (Unit::Str("msg".into()), Unit::Int(out)),
-                    ];
-                    return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-                }
-            }
-
-            if let Operation::Sub(lst) = op {
-                let out = lst.iter().cloned().reduce(|a, b| a - b);
-
-                if let Some(out) = out {
-                    let m = vec![
-                        (Unit::Str("msg".into()), Unit::Int(out)),
-                    ];
-                    return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-                }
-            }
-
-            if let Operation::Pow(lst) = op {
-                let out = lst.iter().cloned().reduce(|a, b| a.pow(b as u32));
-
-                if let Some(out) = out {
-                    let m = vec![
-                        (Unit::Str("msg".into()), Unit::Int(out)),
-                    ];
-                    return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-                }
-            }
-
-            if let Operation::Mul(lst) = op {
-                let out = lst.iter().cloned().reduce(|a, b| a * b);
-
-                if let Some(out) = out {
-                    let m = vec![
-                        (Unit::Str("msg".into()), Unit::Int(out)),
-                    ];
-                    return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-                }
-            }
-
-            if let Operation::Div(lst) = op {
-                let out = lst.iter().cloned().reduce(|a, b| a / b);
-
-                if let Some(out) = out {
-                    let m = vec![
-                        (Unit::Str("msg".into()), Unit::Int(out)),
-                    ];
-                    return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-                }
-            }
-
-            if let Operation::Mod(lst) = op {
-                let out = lst.iter().cloned().reduce(|a, b| a % b);
-
-                if let Some(out) = out {
-                    let m = vec![
-                        (Unit::Str("msg".into()), Unit::Int(out)),
-                    ];
-                    return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
-                }
-            }
+            return Ok(Some(serv.kern.msg(&msg.ath, Unit::Map(m))?))
         }
 
         return Ok(Some(msg))
